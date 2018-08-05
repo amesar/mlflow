230aa5466deae4ef34adc983259d552bf6e1161d
amesar <amesar@users.noreply.github.com>
2018-08-05 15:56:51 +0000

    Java client: LogMetric - double to Double

--- mlflow-java/src/main/java/com/databricks/mlflow/client/objects/LogMetric.java
+++ mlflow-java/src/main/java/com/databricks/mlflow/client/objects/LogMetric.java
@@ -21,10 +21,10 @@ public class LogMetric {
     public String getKey() { return key; }
     public void setKey(String key) { this.key = key; }
  
-    private double value; 
+    private Double value; 
     @JsonProperty("value")
-    public double getValue() { return value; }
-    public void setValue(double value) { this.value = value; }
+    public Double getValue() { return value; }
+    public void setValue(Double value) { this.value = value; }
  
     private String timestamp; 
     @JsonProperty("timestamp")
