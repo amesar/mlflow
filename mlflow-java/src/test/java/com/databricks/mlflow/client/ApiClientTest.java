2caf8a09542f8d3cd19afdc3c909a2265fc3ccd7
amesar <amesar@users.noreply.github.com>
2018-08-05 15:57:09 +0000

    Java client: added test for add run, params and metrics

--- mlflow-java/src/test/java/com/databricks/mlflow/client/ApiClientTest.java
+++ mlflow-java/src/test/java/com/databricks/mlflow/client/ApiClientTest.java
@@ -37,6 +37,74 @@ public class ApiClientTest extends BaseTest {
         Assert.assertEquals(expGet.getArtifactLocation(),expList.getArtifactLocation());
     }
 
+    @Test
+    public void addGetRun() throws Exception {
+        System.out.println("====== createRun"); 
+        // Create exp 
+        String expName = createExperimentName();
+        CreateExperimentResponse expCreate = client.createExperiment(expName);
+        String experimentId = expCreate.getExperimentId();
+    
+        System.out.println("====== createRun");
+        
+        // Create run 
+        String user = System.getenv("USER");
+        long startTime = System.currentTimeMillis();
+        String sourceFile = "MyFile.java";
+        CreateRunRequest request = new CreateRunRequest(experimentId, "run_for_"+experimentId, "LOCAL", sourceFile, startTime, user);     
+        RunInfo runCreated = client.createRun(request);
+        System.out.println("CreateRun: "+runCreated);
+        String runId = runCreated.getRunUuid();
+    
+        // Log parameters
+        client.logParameter(runId, "min_samples_leaf", "2");
+        client.logParameter(runId, "max_depth", "3");
+    
+        // Log metrics
+        client.logMetric(runId, "auc", 2.12);
+        client.logMetric(runId, "accuracy_score", 3.12);
+        client.logMetric(runId, "zero_one_loss", 4.12);
+    
+        // Update finished run
+        UpdateRunRequest update = new UpdateRunRequest(runId, "FINISHED", startTime+1001);
+        client.updateRun(update);
+  
+        // Assert run from getExperiment
+        GetExperimentResponse expResponse = client.getExperiment(expCreate.getExperimentId());
+        Experiment exp = expResponse.getExperiment() ;
+        Assert.assertEquals(exp.getName(),expName);
+        assertRunInfo(expResponse.getRuns().get(0), experimentId, user, sourceFile);
+        
+        // Assert run from getRun
+        GetRunResponse run = client.getRun(runId);
+        System.out.println("GetRun: "+run);
+    
+        RunInfo runInfo = run.getInfo();
+        assertRunInfo(runInfo, experimentId, user, sourceFile);
+  
+        List<Param> params = run.getData().getParams();
+        Assert.assertEquals(params.size(),2);
+        assertParam(params,"min_samples_leaf","2");
+        assertParam(params,"max_depth","3");
+  
+        List<Metric> metrics = run.getData().getMetrics();
+        Assert.assertEquals(metrics.size(),3);
+        assertMetric(metrics,"auc",2.12);
+        assertMetric(metrics,"accuracy_score",3.12);
+        assertMetric(metrics,"zero_one_loss",4.12);
+    }
+
+    private void assertRunInfo(RunInfo runInfo, String experimentId, String user, String sourceName) {
+        Assert.assertEquals(runInfo.getExperimentId(),experimentId);
+        Assert.assertEquals(runInfo.getUserId(),user);
+        Assert.assertEquals(runInfo.getSourceName(),sourceName);
+    }
+    private void assertParam(List<Param> params, String key, String value) {
+        Assert.assertTrue(params.stream().filter(e -> e.getKey().equals(key) && e.getValue().equals(value)).findFirst().isPresent());
+    }
+    private void assertMetric(List<Metric> metrics, String key, double value) {
+        Assert.assertTrue(metrics.stream().filter(e -> e.getKey().equals(key) && e.getValue().equals(value)).findFirst().isPresent());
+    }
     private java.util.Optional<Experiment> getExperimentByName(List<Experiment> exps, String expName) {
         return exps.stream().filter(e -> e.getName().equals(expName)).findFirst();
     }
