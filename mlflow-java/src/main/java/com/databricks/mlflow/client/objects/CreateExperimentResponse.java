e8fb854c0c8e128910f5e5d29e66dd6d476d6767
amesar <amesar@users.noreply.github.com>
2018-08-10 04:20:31 +0000

    Java client: per MLflow 0.4.2: CreateExperimentResponse.java - changed experimentId to experiment_id field per new change of JSON response from server

--- mlflow-java/src/main/java/com/databricks/mlflow/client/objects/CreateExperimentResponse.java
+++ mlflow-java/src/main/java/com/databricks/mlflow/client/objects/CreateExperimentResponse.java
@@ -3,7 +3,7 @@ package com.databricks.mlflow.client.objects;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 public class CreateExperimentResponse {
-    @JsonProperty("experimentId")
+    @JsonProperty("experiment_id")
     private String experimentId;
     public String getExperimentId() { return experimentId; }
     public void setExperimentId(String experimentId) { this.experimentId = experimentId; }
